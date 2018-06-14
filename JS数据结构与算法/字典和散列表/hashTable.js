58c5cf034496a1c272bfabb85ea8360701b0a918
JiangWeixian <saber_wei@163.com>
2018-06-14 14:12:45 +0800

    JS数据结构：哈希表

--- JS数据结构与算法/字典和散列表/hashTable.js
+++ JS数据结构与算法/字典和散列表/hashTable.js
@@ -20,14 +20,32 @@ function HashTable () {
     }
     items[pos].append(new Item(key, value))
   }
-  this.remove = (key)=> {}
+  this.remove = (key)=> {
+    let pos = hashcode(key)
+    let current = items[pos].getHead()
+    while (current) {
+      if (current.element.key === key) {
+        return items[pos].remove(current.element)
+      }
+      current = current.next
+    }
+    return undefined
+  }
   this.get = (key)=> {
-    
+    let pos = hashcode(key)
+    let current = items[pos].getHead()
+    while (current) {
+      if (current.element.key === key) {
+        return current.element.value
+      }
+      current = current.next
+    }
+    return undefined
   }
   this.show = ()=> {
     items.forEach((item)=> {
       if (item) {
-        console.log(item.print())
+        item.print()
       }
     })
   }
@@ -37,3 +55,13 @@ let hashtable = new HashTable()
 hashtable.put('eric', 'bigbigbig')
 hashtable.put('saber', 'smallsmallsmall')
 hashtable.show()
+console.log(hashtable.get('eric'))
+console.log(hashtable.get('crei'))
+console.log('删除eric')
+console.log(hashtable.remove('eric'))
+console.log('删除之后还剩下')
+hashtable.show()
+console.log('检测是否还存在')
+console.log(hashtable.get('eric'))
+console.log('删除不存在的crei')
+console.log(hashtable.remove('crei'))
